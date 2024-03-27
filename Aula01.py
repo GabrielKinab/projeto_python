8f7f5262000aea1bbf1f178fd32f204e6f00b811
GabrielKinab <129771579+GabrielKinab@users.noreply.github.com>
2024-03-27 10:53:57 -0300

    Add files via upload

---
+++ Aula01.py
@@ -0,0 +1,40 @@
+import os 
+
+#print("Bom dia pé-ssoal ou tardesso da noitada!")
+print("Tá ligado que o 3DS, são os caras sagradinhos numero 1 da Escola!")
+turma="3DS"
+pos=2
+print("o", turma, "são os caras sagradinhos numero", pos "da Escola")
+print(type(turma))
+print(type(pos))
+
+os.system("clear")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
